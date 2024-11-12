ec04963eb2c2496147ddbe21791f2a8e49956cc2
Eva_Azizah <effatulazizah@gmail.com>
2024-11-12 08:10:53 +0700

    nambah sm ngubah

--- process.php
+++ process.php
@@ -3,11 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $flight = htmlspecialchars($_POST['flight']);
     $date = htmlspecialchars($_POST['date']);
-    $seast = (int)$_POST['seats'];
+    $seats = (int)$_POST['seats'];
 
-    echo"<h2>Pesanan Anda Berhasil!</h2>";
-    echo"<p>Nama: $name</p>";
-    echo"<p>Penerbangan: $flight</p>";
+    echo "<h2>Pesanan Anda Berhasil!</h2>";
+    echo "<p>Nama: $name</p>";
+    echo "<p>Penerbangan: $flight</p>";
     echo "<p>Tanggal Keberangkatan: $date</p>";
     echo "<p>Jumlah Tiket: $seats</p>";
 }

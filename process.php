f8bfc1fb1c425307f34f1280c9769096cb917972
Eva_Azizah <effatulazizah04@gmail.com>
2025-01-06 12:01:41 +0700

    rubah pembaruan

--- process.php
+++ process.php
@@ -1,29 +1,33 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $flight = htmlspecialchars($_POST['flight']);
-    $date = htmlspecialchars($_POST['date']);
-    $seats = (int)$_POST['seats'];
+session_start();
+$name = htmlspecialchars($_POST['name']);
+$gender = htmlspecialchars($_POST['gender']);
+$flight = htmlspecialchars($_POST['flight']);
+$date = htmlspecialchars($_POST['date']);
+$time = htmlspecialchars($_POST['time']);
+$seats = (int)$_POST['seats'];
 
-    echo "<!DOCTYPE html>
-    <html>
-    <head>
-        <title>Pesanan Anda Berhasil</title>
-        <link rel='stylesheet' href='style.css'>
-    </head>
-    <body>
-        <div class='container'>
-            <h2>Pesanan Anda Berhasil!</h2>
-            <p>Nama : $name</p>
-            <p>Penerbangan : $flight</p>
-            <p>Tanggal Keberangkatan : $date</p>
-            <p>Jumlah Tiket : $seats</p>
-        </div>
-        <footer>
-            <h1>Terima kasih telah bergabung dengan layanan pemesanan tiket kami. Nikmati kemudahan perjalanan dengan berbagai penawaran menarik.</h1>
-            <p>2024-2025 @pesawat.com</p>
-        </footer>
-    </body>
-    </html>";
+$prices = [
+    "Surabaya - Bali" => 500000,
+    "Surabaya - Jakarta" => 800000,
+    "Bali - NTB" => 1000000,
+];
+
+$total_price = $seats * $prices[$flight];
+if (!isset($_SESSION['pesanan'])) {
+    $_SESSION['pesanan'] = [];
 }
+
+$_SESSION['pesanan'][] = [
+    'nama' => $name,
+    'penerbangan' => $flight,
+    'tanggal' => $date,
+    'jam' => $time,
+    'jumlah_tiket' => $seats,
+    'total_harga' => $total_price,
+    'waktu_pesan' => time(),
+];
+
+header("location: process_pesanan.php");
+exit();
 ?>
